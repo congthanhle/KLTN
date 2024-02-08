394e0b72be5eb588f8a39ad73047b99f9989dde9
thanhlc <thanhlc@runsystem.net>
2024-02-08 17:12:53 +0700

    feat: config

--- thesis/src/app/index.tsx
+++ thesis/src/app/index.tsx
@@ -1,12 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, FlatList } from 'react-native';
+import ListItem from '@/components/core/ListItem';
+
+const features = [
+  {
+    index: 1,
+    name: "Camera",
+    path: "/camera"
+  },
+  {
+    index: 2,
+    name: "Health records",
+    path: "/healthRecord"
+  }
+]
 
 export default function index() {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+    <View style={styles.container}>
+    <FlatList
+      data={features}
+      contentContainerStyle={styles.content}
+      columnWrapperStyle={styles.column}
+      numColumns={2}
+      renderItem={({ item }) => <ListItem feature={item} />}
+    />
+
+    <StatusBar style="auto" />
+  </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+
+  content: {
+    gap: 10,
+    padding: 10,
+  },
+  column: {
+    gap: 10,
+  },
+});
