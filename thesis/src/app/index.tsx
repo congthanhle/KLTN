0b50b0ef25194c3cdced51c14090f05d8d88302a
thanhlc <thanhlc@runsystem.net>
2024-02-14 23:14:55 +0700

    feat: menu

--- thesis/src/app/index.tsx
+++ thesis/src/app/index.tsx
@@ -1,47 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, FlatList } from 'react-native';
-import ListItem from '@/components/core/ListItem';
+import React from "react";
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  Pressable,
+  Image,
+} from "react-native";
+import { Link } from "expo-router";
+import { Color } from "@utils/TsColorConstant";
 
-const features = [
-  {
-    index: 1,
-    name: "Camera",
-    path: "/camera"
-  },
-  {
-    index: 2,
-    name: "Health records",
-    path: "/healthRecord"
-  }
-]
-
-export default function index() {
+const App = () => {
   return (
-    <View style={styles.container}>
-    <FlatList
-      data={features}
-      contentContainerStyle={styles.content}
-      columnWrapperStyle={styles.column}
-      numColumns={2}
-      renderItem={({ item }) => <ListItem feature={item} />}
-    />
-
-    <StatusBar style="auto" />
-  </View>
+    <SafeAreaView style={styles.container}>
+      <Link href={`/camera`} asChild>
+        <Pressable style={{ ...styles.box, backgroundColor: Color.COLOR_04 }}>
+          <Text style={styles.text}>Scan</Text>
+          <Image
+            style={styles.stretch}
+            source={require("@assets/img/common/scan.png")}
+          />
+        </Pressable>
+      </Link>
+      <Link href={`/healthRecord`} asChild>
+        <Pressable style={{ ...styles.box, backgroundColor: Color.COLOR_04 }}>
+          <Text style={styles.text}>Health records</Text>
+          <Image
+            style={styles.stretch}
+            source={require("@assets/img/common/stethoscope.png")}
+          />
+        </Pressable>
+      </Link>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: StatusBar.currentHeight || 0,
   },
-
-  content: {
-    gap: 10,
-    padding: 10,
+  box: {
+    height: 80,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#9b4521",
+    borderRadius: 20,
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 10,
+    marginHorizontal: 16,
+    flexDirection: "row",
+    paddingHorizontal: 32,
   },
-  column: {
-    gap: 10,
+  text: {
+    color: Color.COLOR_01,
+    fontSize: 20,
+  },
+  stretch: {
+    width: 50,
+    height: 50,
   },
 });
+
+export default App;
